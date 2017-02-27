4df72e64c336c052aed39fb829ea5151aac7afcc
Adam Gapinski <gapinski.adam@outlook.com>
2017-02-27 23:04:11 +0100

    Created listener interfaces, that will be implemented by User class to notify the user model when some message or new channel was created

---
+++ src/main/java/com/adam58/model/IChannelsListener.java
@@ -0,0 +1,8 @@
+package com.adam58.model;
+
+/**
+ * @author Adam Gapiński
+ */
+public interface IChannelsListener {
+    void receiveChannel(IChannel channel);
+}
---
+++ src/main/java/com/adam58/model/IMessageListener.java
@@ -0,0 +1,8 @@
+package com.adam58.model;
+
+/**
+ * @author Adam Gapiński
+ */
+public interface IMessageListener {
+    void receiveMessage(Message message);
+}
