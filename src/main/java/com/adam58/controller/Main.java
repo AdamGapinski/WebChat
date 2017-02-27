e9383f3449cf9174ac9e00d3fa3dd49af669efe3
Adam Gapinski <adam.gapinski1@gmail.com>
2017-02-27 13:19:16 +0100

    Added comments in Main class

--- src/main/java/com/adam58/controller/Main.java
+++ src/main/java/com/adam58/controller/Main.java
@@ -1,28 +1,35 @@
 package com.adam58.controller;
 
-
-import static spark.Spark.*;
+import spark.Spark;
 
 /**
  * @author Adam Gapiński
+ *
+ * Main class serves as entry point for the whole application. Main mthod initializes Spark framework and set
+ * up web application which is accessible at localhost:4567
  */
 public class Main {
     public static void main(String[] args) {
         Chat chat = new Chat();
-        webSocket("/index", new ChatMenuSocketHandler(chat));
-        webSocket("/channel", new ChannelSocketHandler(chat));
+
+        /*
+        * webSocket method registers java class to handle javascript web socket requests. Web socket
+        * is accessible at given url path
+        * */
+        Spark.webSocket("/index", new ChatMenuSocketHandler(chat));
+        Spark.webSocket("/channel", new ChannelSocketHandler(chat));
         setStaticFileLocation();
 
-        init();
+        Spark.init();
     }
 
     private static void setStaticFileLocation() {
         String projectDir;
         if ((projectDir = System.getProperty("user.dir")).contains("IdeaProjects")) {
             String staticDir = "/src/main/resources/public";
-            externalStaticFileLocation(projectDir + staticDir);
+            Spark.externalStaticFileLocation(projectDir + staticDir);
         } else {
-            staticFileLocation("/public");
+            Spark.staticFileLocation("/public");
         }
     }
 }
