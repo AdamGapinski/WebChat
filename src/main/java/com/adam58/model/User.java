7b0802fd3c3f906f2f9df404e8bfa23588a9e771
Adam Gapinski <gapinski.adam@outlook.com>
2017-04-09 12:43:21 +0200

    User model class with equals and hashCode implemented

--- src/main/java/com/adam58/model/User.java
+++ src/main/java/com/adam58/model/User.java
@@ -4,7 +4,7 @@ package com.adam58.model;
  * @author Adam Gapiński
  */
 public class User {
-    public final String username;
+    private final String username;
 
     public User(String username) {
         this.username = username;
@@ -12,5 +12,20 @@ public class User {
 
     public String getUsername() {
         return username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return username != null ? username.equals(user.username) : user.username == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return username != null ? username.hashCode() : 0;
     }
 }
