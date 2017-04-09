4e4bdb1990bd45d51913cd7791e1682116f022e2
Adam Gapinski <gapinski.adam@outlook.com>
2017-04-09 12:45:15 +0200

    Created ChannelView class and modified corresponding interface. It sends data (like messages) to attached session

---
+++ src/main/java/com/adam58/View/ChannelView.java
@@ -0,0 +1,70 @@
+package com.adam58.View;
+
+import com.adam58.model.Message;
+import j2html.tags.ContainerTag;
+import org.eclipse.jetty.websocket.api.Session;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static j2html.TagCreator.*;
+
+/**
+ * @author Adam Gapiński
+ *
+ * ChannelView class is responsible for presenting channel data (like messages) to users. Using session interface
+ * it sends html, which then is inserted to page by javascript.
+ */
+public class ChannelView implements IChannelView {
+    private Session session;
+
+    public ChannelView(Session session) {
+        this.session = session;
+    }
+
+    @Override
+    public void receiveMessage(Message message) {
+        try {
+            session.getRemote().sendString(String.valueOf(new JSONObject()
+                    .put("message", createHtmlMessageFromSender(message.getSender(), message.getContent()))
+                    .put("userlist", new ArrayList<>())// TODO: 03.03.17 user list
+            ));
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void showMessages(List<Message> messages) {
+        messages.forEach(message -> {
+            try {
+                session.getRemote().sendString(String.valueOf(new JSONObject()
+                                .put("message", createHtmlMessageFromSender(message.getSender(), message.getContent()))
+                                .put("userlist", new ArrayList<>())
+                        ));
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private String createHtmlMessageFromSender(String sender, String message) {
+        ContainerTag result = article();
+
+        if (sender.toLowerCase().equals("server")) {
+            result.withId("serversender");
+        }
+
+        // TODO: 03.03.17 Message timestamp should be get from the message object
+        return result.with(
+                b(sender + " says:"),
+                p(message),
+                span().withClass("timestamp").withText(new SimpleDateFormat("HH:mm:ss").format(new Date()))
+        ).render();
+    }
+}
--- src/main/java/com/adam58/View/IChannelView.java
+++ src/main/java/com/adam58/View/IChannelView.java
@@ -2,8 +2,6 @@ package com.adam58.View;
 
 import com.adam58.model.IMessageListener;
 import com.adam58.model.Message;
-import com.adam58.model.User;
-import org.eclipse.jetty.websocket.api.Session;
 
 import java.util.List;
 
@@ -11,8 +9,5 @@ import java.util.List;
  * @author Adam Gapiński
  */
 public interface IChannelView extends IMessageListener {
-    void registerUserSession(User user, Session session);
-    User unregisterUserSession(Session session);
-    void showMessages(List<Message> messages, Session session);
-    boolean userHasAnySession(User user);
+    void showMessages(List<Message> messages);
 }
